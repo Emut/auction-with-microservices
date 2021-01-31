6444b096c294cd5ba73a69db5d3f7e556158be13
Emut <numutekici@gmail.com>
2021-01-31 15:47:45 +0300

    updated startup script

--- start_services.sh
+++ start_services.sh
@@ -8,3 +8,9 @@ java -jar ./userservice/target/userservice-0.0.1-SNAPSHOT.jar --server.port=9003
 java -jar ./inventoryservice/target/inventoryservice-0.0.1-SNAPSHOT.jar --server.port=9101 --application.id=1 &
 java -jar ./inventoryservice/target/inventoryservice-0.0.1-SNAPSHOT.jar --server.port=9102 --application.id=2 &
 java -jar ./inventoryservice/target/inventoryservice-0.0.1-SNAPSHOT.jar --server.port=9103 --application.id=3 &
+
+java -jar ./auctionservice/target/auctionservice-0.0.1-SNAPSHOT.jar --server.port=9201 --application.id=1 &
+java -jar ./auctionservice/target/auctionservice-0.0.1-SNAPSHOT.jar --server.port=9202 --application.id=2 &
+
+java -jar ./paymentservice/target/paymentservice-0.0.1-SNAPSHOT.jar --server.port=9301 --application.id=1 &
+java -jar ./paymentservice/target/paymentservice-0.0.1-SNAPSHOT.jar --server.port=9302 --application.id=2 &
